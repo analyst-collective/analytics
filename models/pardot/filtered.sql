ae3fab7069c9c28db26f530bba9b7051f159262f
Tristan Handy <jthandy@gmail.com>
2016-02-23 15:17:52 -0500

    all tiers of pardot models compile.

--- models/pardot/filtered.sql
+++ models/pardot/filtered.sql
@@ -23,72 +23,12 @@ create or replace view pardot.prospect_filtered as (
 
 
 
-create or replace view pardot.campaign_filtered as (
+create or replace view pardot.opportunity_filtered as (
 
 	select
 		*
 	from
-		pardot.campaign_base
-  --where
-
-);
-
-
-
-create or replace view pardot.campaign_filtered as (
-
-	select
-		*
-	from
-		pardot.campaign_base
-  --where
-
-);
-
-
-
-create or replace view pardot.visit_filtered as (
-
-	select
-		*
-	from
-		pardot.visit_base
-  --where
-
-);
-
-
-
-create or replace view pardot.visitor_filtered as (
-
-	select
-		*
-	from
-		pardot.visitor_base
-  --where
-
-);
-
-
-
-create or replace view pardot.visitor_pageview_filtered as (
-
-	select
-		*
-	from
-		pardot.visitor_pageview_base
-  --where
-
-);
-
-
-
-create or replace view pardot.visitor_referrer_filtered as (
-
-	select
-		*
-	from
-		pardot.visitor_referrer_base
+		pardot.opportunity_base
   --where
 
 );
--- models/pardot/transformed.sql
+++ models/pardot/transformed.sql
@@ -1,4 +1,4 @@
-create or replace view pardot.visitor_activity_transformed as (
+create or replace view pardot_analysis.visitoractivity_transformed as (
 	--this table has a bunch of types that really should be event actions but are very poorly formulated.
 	--the custom logic in this view is an attempt to fix that.
 	--not all of the various type / type_name combinations have been accounted for yet; I still need to determine exactly what some of them mean.
@@ -88,11 +88,11 @@ create or replace view pardot.visitor_activity_transformed as (
 	    when type = 18 and type_name = '' then 'reopened opportunity'
 	  end as event_action
 	from
-		olga_pardot.visitoractivity
+		pardot_analysis.visitoractivity_filtered
 
 );
 
-create or replace view pardot.prospect_transformed as (
+create or replace view pardot_analysis.prospect_transformed as (
 
 	select
 		id 											as id,
@@ -102,13 +102,11 @@ create or replace view pardot.prospect_transformed as (
 		last_name 							as last_name,
 		email 									as email,
 		company 								as company,
-		prospect_account_id 		as prospect_account_id,
 		website 								as website,
 		job_title 							as job_title,
 		department 							as department,
 		country 								as country,
 		address_one 						as address_one,
-		address_two 						as address_two,
 		city 										as city,
 		state 									as state,
 		territory 							as territory,
@@ -117,9 +115,7 @@ create or replace view pardot.prospect_transformed as (
 		fax 										as fax,
 		source 									as source,
 		annual_revenue 					as annual_revenue,
-		employees 							as employees,
 		industry 								as industry,
-		years_in_business 			as years_in_business,
 		comments 								as comments,
 		notes 									as notes,
 		score 									as score,
@@ -129,7 +125,6 @@ create or replace view pardot.prospect_transformed as (
 		crm_lead_fid 						as crm_lead_fid,
 		crm_contact_fid 				as crm_contact_fid,
 		crm_owner_fid 					as crm_owner_fid,
-		crm_account_fid 				as crm_account_fid,
 		crm_last_sync 					as crm_last_sync,
 		crm_url 								as crm_url,
 		is_do_not_email 				as is_do_not_email,
@@ -140,24 +135,12 @@ create or replace view pardot.prospect_transformed as (
 		created_at 							as created_at,
 		updated_at 							as updated_at
 	from
-		olga_pardot.prospect
+		pardot_analysis.prospect_filtered
 
 );
 
 
-create or replace view pardot.campaign_transformed as (
-
-	select
-		id													as id,
-		cost												as cost,
-		name												as name
-	from
-		olga_pardot.campaign
-
-);
-
-
-create or replace view pardot.opportunity_transformed as (
+create or replace view pardot_analysis.opportunity_transformed as (
 
 	select
 		probability									as probability,
@@ -172,71 +155,6 @@ create or replace view pardot.opportunity_transformed as (
 		stage												as stage,
 		"type"											as "type"
 	from
-		olga_pardot.opportunity
-
-);
-
-
-create or replace view pardot.visit_transformed as (
-
-	select
-		duration_in_seconds					as duration_in_seconds,
-		id													as id,
-		last_visitor_page_view_at		as last_visitor_page_view_at,
-		updated_at									as updated_at,
-		first_visitor_page_view_at	as first_visitor_page_view_at,
-		visitor_page_view_count			as visitor_page_view_count,
-		created_at									as created_at,
-		visitor_id									as visitor_id,
-		content_parameter						as content_parameter,
-		term_parameter							as term_parameter,
-		medium_parameter						as medium_parameter,
-		source_parameter						as source_parameter,
-		prospect_id									as prospect_id,
-		campaign_parameter					as campaign_parameter
-	from
-		olga_pardot.visit
-
-);
-
-
-create or replace view pardot.visitor_transformed as (
-
-	select
-		id													as id,
-		prospect_id									as prospect_id
-	from
-		olga_pardot.visitor_filtered
-
-);
-
-
-create or replace view pardot.visitor_pageview_transformed as (
-
-	select
-		url													as url,
-		id													as id,
-		visit_id										as visit_id,
-		title												as title,
-		created_at									as created_at,
-		visitor_id									as visitor_id
-	from
-		pardot.visitor_pageview_filtered
-
-);
-
-
-create or replace view pardot.visitor_referrer_transformed as (
-
-	select
-		vendor											as vendor,
-		referrer										as referrer,
-		id													as id,
-		"type"											as "type",
-		visitor_id									as visitor_id,
-		query												as query,
-		prospect_id									as prospect_id
-	from
-		pardot.visitor_referrer_filtered
+		pardot_analysis.opportunity_filtered
 
 );
