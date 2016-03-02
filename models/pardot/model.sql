314e058079914cc8183496e530a5c53becf0f377
Drew Banin <dsb59@drexel.edu>
2016-03-02 13:21:28 -0500

    prune useless code

--- models/pardot/model.sql
+++ models/pardot/model.sql
@@ -1,9 +1,12 @@
+drop schema if exists pardot_analysis cascade;
+create schema pardot_analysis;
+
 create or replace view pardot_analysis.visitoractivity_types_meta as (
 
   --these literal values are pulled from pardot's api docs here:
   --http://developer.pardot.com/kb/object-field-references/#visitor-activity
   --they change periodically over time and this query will need to be correspondingly modified.
-  select 1 as "type", 'Click' as type_decoded union all
+  select 1 as type, 'Click' as type_decoded union all
   select 2, 'View' union all
   select 3, 'Error' union all
   select 4, 'Success' union all
@@ -36,7 +39,7 @@ create or replace view pardot_analysis.visitoractivity_types_meta as (
   select 31, 'UserVoice Suggestion' union all
   select 32, 'UserVoice Comment' union all
   select 33, 'UserVoice Ticket' union all
-  select 34, 'Video Watched (>= 75% watched)'
+  select 34, 'Video Watched (≥ 75% watched)'
 
 );
 
@@ -81,3 +84,24 @@ create or replace view pardot_analysis.visitoractivity_events_meta as (
   select 18, '', 'reopened opportunity'
 
 );
+
+
+create or replace view pardot_analysis.visitoractivity as (
+  --this table has a bunch of types that really should be event actions but are very poorly formulated.
+  --the custom logic in this view is an attempt to fix that.
+  --not all of the various type / type_name combinations have been accounted for yet; I still need to determine exactly what some of them mean.
+  select
+    -- event_stream interface
+    va.created_at       as timestamp,
+    t.type_decoded      as event,
+    va.prospect_id      as user_id,
+    va.*
+  from
+    pardot_analysis.visitoractivity_filtered va
+    inner join pardot_analysis.visitoractivity_events_meta e
+      on va."type" = e."type" and va.type_name = e.type_name
+    inner join pardot_analysis.visitoractivity_types_meta t
+      on va."type" = t."type"
+);
+
+COMMENT ON VIEW pardot_analysis.visitoractivity IS 'timeseries,funnel,cohort';
--- models/segment/model.sql
+++ models/segment/model.sql
@@ -3,107 +3,13 @@ create schema segment_analysis;
 
 CREATE OR REPLACE VIEW segment_analysis.track as (
   SELECT
-    "timestamp"                  as "timestamp",
-    "event"                      as "event_name",
-    "type"                       as "event_type",
+    "timestamp"  as "timestamp",
+    "event"      as "event",
+    "userid"     as "user_id",
+    *
 
-    "anonymousid"                as "anonymousid",
-    "channel"                    as "channel",
-    "context__library__version"  as "context__library__version"
-    "context__page__path"        as "context__page__path",
-    "context__page__search"      as "context__page__search",
-    "context__page__title"       as "context__page__title",
-    "context__page__url"         as "context__page__url",
-    "event"                      as "event",
-    "messageid"                  as "messageid",
-    "originaltimestamp"          as "originaltimestamp",
-    "projectid"                  as "projectid",
-    "receivedat"                 as "receivedat",
-    "sentat"                     as "sentat",
-    "type"                       as "type",
-    "userid"                     as "userid",
-    "version"                    as "version",
   FROM
     segment.track
 );
 
 COMMENT ON VIEW segment_analysis."track" IS 'timeseries,funnel,cohort';
-
-
-CREATE OR REPLACE VIEW segment_analysis."group" as (
-  SELECT
-    "anonymousid"                as "anonymousid",
-    "channel"                    as "channel",
-    "context__library__version"  as "context__library__version"
-    "context__page__path"        as "context__page__path",
-    "context__page__search"      as "context__page__search",
-    "context__page__title"       as "context__page__title",
-    "context__page__url"         as "context__page__url",
-    "groupid"                    as "groupid",
-    "messageid"                  as "messageid",
-    "originaltimestamp"          as "originaltimestamp",
-    "projectid"                  as "projectid",
-    "receivedat"                 as "receivedat",
-    "sentat"                     as "sentat",
-    "timestamp"                  as "timestamp",
-    "traits__name"               as "traits__name",
-    "type"                       as "type",
-    "userid"                     as "userid",
-    "version"                    as "version",
-  FROM
-    segment."group"
-);
-
-
-
-CREATE OR REPLACE VIEW segment_analysis.identify as (
-  SELECT
-    "anonymousid"                as "anonymousid",
-    "channel"                    as "channel",
-    "context__library__version"  as "context__library__version"
-    "context__page__path"        as "context__page__path",
-    "context__page__search"      as "context__page__search",
-    "context__page__title"       as "context__page__title",
-    "context__page__url"         as "context__page__url",
-    "messageid"                  as "messageid",
-    "originaltimestamp"          as "originaltimestamp",
-    "projectid"                  as "projectid",
-    "receivedat"                 as "receivedat",
-    "sentat"                     as "sentat",
-    "timestamp"                  as "timestamp",
-    "type"                       as "type",
-    "userid"                     as "userid",
-    "version"                    as "version",
-  FROM
-    segment.identify
-);
-
-
-
-CREATE OR REPLACE VIEW segment_analysis.page as (
-  SELECT
-    "timestamp"                  as "timestamp",
-    "type"                       as "event_type",
-    'pageview'                   as "event_name",
-
-    "anonymousid"                as "anonymousid",
-    "channel"                    as "channel",
-    "context__library__version"  as "context__library__version"
-    "context__page__path"        as "context__page__path",
-    "context__page__search"      as "context__page__search",
-    "context__page__title"       as "context__page__title",
-    "context__page__url"         as "context__page__url",
-    "messageid"                  as "messageid",
-    "originaltimestamp"          as "originaltimestamp",
-    "projectid"                  as "projectid",
-    "receivedat"                 as "receivedat",
-    "sentat"                     as "sentat",
-    "timestamp"                  as "timestamp",
-    "type"                       as "type",
-    "userid"                     as "userid",
-    "version"                    as "version",
-  FROM
-    segment.page
-);
-
-COMMENT ON VIEW segment_analysis."page" IS 'timeseries,funnel,cohort';
