f19c58ba3853ef808e61d866dbf813f17b11efd0
Henry Hund <henryhund@gmail.com>
2016-03-14 18:23:22 -0400

    define adwords_summary schema

---
+++ models/adwords/model.sql
@@ -0,0 +1,25 @@
+create or replace view {schema}.adwords_summary as (
+  with ad_data as
+    (
+    select 
+      lower(addestinationurl) as cleanurl,
+      * 
+    from 
+      adwords.adwords12218869_v2 -- How do I make this work regardless of the extension?
+    )
+  select 
+    REPLACE(REGEXP_SUBSTR(cleanurl,'utm_source=[^&]*'),'utm_source=','') as "@utm_source",
+    REPLACE(REGEXP_SUBSTR(cleanurl,'utm_medium=[^&]*'),'utm_medium=','') as "@utm_medium",
+    REPLACE(REGEXP_SUBSTR(cleanurl,'utm_campaign=[^&]*'),'utm_campaign=','') as "@utm_campaign",
+    REPLACE(REGEXP_SUBSTR(cleanurl,'utm_content=[^&]*'),'utm_content=','') as "@utm_content",
+    REPLACE(REGEXP_SUBSTR(cleanurl,'utm_term=[^&]*'),'utm_term=','') as "@utm_term",
+    impressions::integer as "@impressions",
+    adcost::float as "@cost",
+    date::date as "@date",
+    adclicks::integer as "@clicks",
+    SPLIT_PART(cleanurl,'?',1) as "@base_url",
+    SPLIT_PART(cleanurl,'?',2) as querystring,
+    *
+  from 
+    ad_data
+  )
