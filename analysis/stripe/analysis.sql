fc9b2ec5102476f6448f4f60ce942dc4e25f1e46
Tristan Handy <jthandy@gmail.com>
2016-03-23 17:05:42 -0400

    major refactor of stripe analysis in response to comments in PR

--- analysis/stripe/analysis.sql
+++ analysis/stripe/analysis.sql
@@ -1,79 +1,109 @@
-with
+--bugs:
+--for monthly customers who are missing payments, this query records the upgrade when they make the payment but not the prior downgrade when they miss the payment.
+--i'm not confident that upgrades and downgrades are having the appropriate amount of revenue called 'renewal' and the appropriate called 'upgrade' or 'downgrade'.
+--aka, if a person upgrades from 5 to 50, that's a 5 renewal and a 45 upgrade. from 50 to 5 that's a 45 downgrade and a 5 renewal.
+--in general, i would greatly appreciate just some data validation here, making sure that we are extremely confident that the final query here is accurate.
 
-invoices as (
+with invoices as (
+
   select *
-  from ac_jthandy.stripe_invoices_transformed
-),
+  from {{env.schema}}.stripe_invoices_transformed
 
-plan_changes as (
+), all_months as (
+
+  select distinct date_month from invoices
+
+), plan_changes as (
+
   select
-    invoices.customer as customer,
-    date_trunc('month', invoices.date) as month,
-    lag(invoices.total) over (partition by invoices.customer order by invoices.date) as before,
-    invoices.total as after,
-    invoices.total - lag(invoices.total) over (partition by invoices.customer order by invoices.date) as change
+    i.*,
+    pmi.total as prior_month_total,
+    i.total - coalesce(pmi.total, 0) as change,
+    pmi.period_end as prior_month_period_end
   from
-    invoices
-),
+    invoices i
+    left outer join invoices pmi
+    on i.date_month = dateadd('month', 1, pmi.date_month)
+      and i.customer = pmi.customer
 
-news as (
-  select
-    date_trunc('month', date) as month,
-    sum(total) as total
-  from invoices
-  where first_payment = 1
-  group by 1
-),
+), data as (
 
-churns AS (
-  select
-    date_trunc('month', invoices.date) as month,
-    sum(total) as total
-  from invoices
-  where last_payment = 1
-  and date_trunc('month', date) < date_trunc('month', current_date)
-  group by 1
-),
-
-upgrades as (
-  select
-    month,
-    sum(change) as total
+  select date_month, total, change,
+    case
+      when first_payment = 1
+        then 'new'
+      when last_payment = 1
+        and dateadd('month', 1, period_end) < current_date
+        then 'churn'
+      when change > 0
+        then 'upgrade'
+      when change < 0
+        then 'downgrade'
+      when period != 'monthly'
+        and date_month < date_trunc('month', prior_month_period_end)
+        then 'prepaid renewal'
+      else
+        'renewal'
+      end revenue_category
   from plan_changes
-  where change > 0
-  group by 1
-),
+  order by customer, payment_number
 
-downgrades as (
-  select
-    month          as month,
-    sum(change)    as total
-  from plan_changes
-  where change < 0
+), news as (
+
+  select date_month, sum(total) as value
+  from data
+  where revenue_category = 'new'
   group by 1
+
+), renewals as (
+
+  select date_month, sum(total) as value
+  from data
+  where revenue_category in ('renewal', 'downgrade', 'upgrade')
+  group by 1
+
+), prepaids as (
+
+  select date_month, sum(total) as value
+  from data
+  where revenue_category = 'prepaid renewal'
+  group by 1
+
+), churns as (
+
+  select date_month, sum(total) as value
+  from data
+  where revenue_category = 'churn'
+  group by 1
+
+), upgrades as (
+
+  select date_month, sum(change) as value
+  from data
+  where revenue_category = 'upgrade'
+  group by 1
+
+), downgrades as (
+
+  select date_month, sum(change) as value
+  from data
+  where revenue_category = 'downgrade'
+  group by 1
+
 )
 
-
-select
-  news.month,
-  coalesce(news.total,0) as news,
-  coalesce(churns.total,0)*-1 as churn,
-  upgrades.total as upgrades,
-  coalesce(downgrades.total,0) as downgrades,
-  case news.month
-    when date_trunc('month', date_trunc('month', current_date) - 1)
-      then 0
-    else
-      case coalesce(churns.total, 0)
-      when 0
-        then 20
-      else
-        coalesce(news.total, 0)::float / coalesce(churns.total, 0)::float
-      end
-    end as quickratio
-from news
-  left outer join upgrades on news.month = upgrades.month
-  left outer join downgrades on news.month = downgrades.month
-  left outer join churns on news.month = churns.month
-where news.month < date_trunc('month', current_date)
-order by month
+select all_months.date_month,
+  news.value as new,
+  renewals.value as renewal,
+  prepaids.value as committed,
+  churns.value * -1 as churned,
+  upgrades.value as upgrades,
+  downgrades.value as downgrades
+from all_months
+  left outer join news on all_months.date_month = news.date_month
+  left outer join renewals on all_months.date_month = renewals.date_month
+  left outer join prepaids on all_months.date_month = prepaids.date_month
+  left outer join churns on all_months.date_month = churns.date_month
+  left outer join upgrades on all_months.date_month = upgrades.date_month
+  left outer join downgrades on all_months.date_month = downgrades.date_month
+order by 1;
--- model/stripe/model.sql
+++ model/stripe/model.sql
@@ -6,9 +6,27 @@ create or replace view {{env.schema}}.stripe_invoices as (
     forgiven,
     subscription as subscription_id,
     paid,
-    total
+    total,
+    period_start,
+    period_end
   from
     demo_data.stripe_invoices
+
+);
+
+create or replace view {{env.schema}}.stripe_invoices_cleaned as (
+
+  select
+    customer,
+    timestamp 'epoch' + date * interval '1 Second' as date,
+    forgiven,
+    subscription_id,
+    paid,
+    total,
+    timestamp 'epoch' + period_start * interval '1 Second' as period_start,
+    timestamp 'epoch' + period_end * interval '1 Second' as period_end
+  from
+    {{env.schema}}.stripe_invoices
 
 );
 
@@ -37,51 +55,59 @@ create or replace view {{env.schema}}.stripe_plans as (
 
 create or replace view {{env.schema}}.stripe_invoices_transformed as (
 
-  with data as (
+  with invoices as (
 
-    select
-        i.customer                as customer,
-        timestamp 'epoch' + i.date * interval '1 Second'        as date,
-        i.forgiven                as forgiven,
-        i.paid                    as paid,
-        case  p.interval
-          when 'yearly'
-          then i.total::float / 12 / 100
-          else i.total::float / 100
-        end                                                       as total,
-        row_number() over(
-          partition by i.customer
-          order by i.date desc
-        )                                                         as inverse_payment_number,
-        row_number() over(
-          partition by i.customer
-          order by i.date asc
-        )                                                         as payment_number
-      from
-        {{env.schema}}.stripe_invoices i
-      join
-        {{env.schema}}.stripe_subscriptions s
-      on
-        i.subscription_id = s.id
-      join
-        {{env.schema}}.stripe_plans p
-      on
-        s.plan_id = p.id
+    select *
+    from {{env.schema}}.stripe_invoices_cleaned
+
+  ), d1 as (
+
+    select (min(period_start) over () + row_number() over ())::date as date_day
+    from invoices
+
+  ), dates as (
+
+    select distinct date_trunc('month', date_day)::date as date_month
+    from d1
+    where date_day <= current_date
+
+  ), data as (
+
+    select date_month, period_start, period_end,
+      "interval" as period, customer,
+      case  "interval"
+        when 'yearly'
+        then i.total::float / 12 / 100
+        else i.total::float / 100
+      end as total,
+      row_number() over(
+        partition by i.customer
+        order by d.date_month) as payment_number
+    from dates d
+      inner join invoices i
+        on d.date_month::timestamp >= date_trunc('month', i.period_start)
+        and d.date_month::timestamp < date_trunc('month', i.period_end)
+      inner join {{env.schema}}.stripe_subscriptions s on i.subscription_id = s.id
+      inner join {{env.schema}}.stripe_plans p on s.plan_id = p.id
+    where paid is true
+      and forgiven is false
+    order by customer, date_month
 
   )
 
-  select customer, date, total, payment_number,
-    case inverse_payment_number
-      when 1 then 1
-      else 0
-    end as last_payment,
-    case payment_number
-      when 1 then 1
-      else 0
-    end as first_payment
-  from data
-  where
-    forgiven is not true
-    and paid is true
+    select customer, date_month, total, payment_number, period_end, period,
+      case
+        when date_month = last_value(date_month)
+          over (partition by customer
+              order by payment_number
+              rows between unbounded preceding and unbounded following)
+        then 1
+        else 0
+      end as last_payment,
+      case payment_number
+        when 1 then 1
+        else 0
+      end as first_payment
+    from data
 
 );
