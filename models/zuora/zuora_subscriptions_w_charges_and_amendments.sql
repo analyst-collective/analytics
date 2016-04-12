c4aaaa76efc46b2891c6cf804d00c9d355d0b34c
David Wallace <dwallace@rjmetrics.com>
2016-04-12 14:51:39 -0400

    update zuora subscriptions with charges and amendments model sql

--- models/zuora/zuora_subscriptions_w_charges_and_amendments.sql
+++ models/zuora/zuora_subscriptions_w_charges_and_amendments.sql
@@ -2,15 +2,15 @@
 with subscr_w_amendments as
 (
     select
-        account_number, acc.account_id, sub.subscr_id,
-        subscr_name, subscr_status, subscr_term_type, 
-        subscr_start, subscr_end, subscr_version, amend_id, amend_start
+        account_number, acc.account_id, sub.sub_id,
+        sub_name, status, termtype, 
+        sub_start_date, sub_end_date, sub_version, amend_id, amend_start
     from {{env.schema}}.zuora_account acc
     inner join {{env.schema}}.zuora_subscription sub
-        on acc.account_id = sub.account_id
+        on acc.account_id = sub.accountid
     -- add ammendments
     left outer join {{env.schema}}.zuora_amendment amend
-        on sub.subscr_id = amend.subscr_id
+        on sub.sub_id = amend.subscr_id
 )
 
 select
