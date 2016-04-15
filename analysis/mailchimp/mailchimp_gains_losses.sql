0ff459c95c629e37e9a9c060c34a2fced15dcbc4
Tristan Handy <jthandy@gmail.com>
2016-04-15 12:58:11 -0400

    updates based on pr

--- analysis/mailchimp/mailchimp_gains_losses.sql
+++ analysis/mailchimp/mailchimp_gains_losses.sql
@@ -54,7 +54,7 @@ monthly_gains_and_losses as (
 
   select
   	months.month, nvl(total_gains,0) as month_gains,
-  	nvl(total_losses,0) as month_losses
+  	-nvl(total_losses,0) as month_losses
   from months
   left outer join gains
   	on months.month = gains.month
@@ -64,9 +64,8 @@ monthly_gains_and_losses as (
 
 )
 
-select month, month_gains, month_losses * -1 as month_losses, month_gains - month_losses as net_member_growth,
+select month, month_gains, month_losses, month_gains + month_losses as net_member_growth,
 	sum(month_gains) over (order by month rows unbounded preceding) as cum_gains,
 	sum(month_losses) over (order by month rows unbounded preceding) as cum_losses,
-	sum(month_gains) over (order by month rows unbounded preceding) -
-	sum(month_losses) over (order by month rows unbounded preceding) as active_members
+	sum(month_gains + month_losses) over (order by month rows unbounded preceding) as active_members
 from monthly_gains_and_losses
